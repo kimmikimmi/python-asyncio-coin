142a67843702809fd355e2f9840e9b013aa97cb8
bright2013 <bright2013@coupang.com>
2018-10-21 00:02:13 +0900

    1. refactor coin_service 2. buy and selling bug fix. 3. change max_instances size to 2

--- coin_service.py
+++ coin_service.py
@@ -1,91 +1,86 @@
 from datetime import datetime
 import pybithumb
 
+##############################################################################################################################
 con_key = ""
 sec_key = ""
 
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
-coin_dic = bithumb.get_tickers()
-price = bithumb.get_current_price("BTC")
-print(price)
+coin_cache = bithumb.get_tickers()
+#coin_cache.remove("BTC")
+#change coin_cache if u wanna test it
 bucket = {}
-balance2 = bithumb.get_balance("TRX")
 
-print(balance2[2])
+times_for_sell = 2
+times_for_buy = 1.2
 
+
+##############################################################################################################################
 
 def check_new_coin():
     recent = get_all()
 
-    if len(recent) < len(coin_dic):
-        for coin in coin_dic:
-            if coin not in recent:
-                print("신규 상장 코인이 있습니다" + coin)
-                bucket[coin] = bithumb.get_current_price(coin)
-                coin_dic.append(coin)  # 신규 코인 추가
-                return coin
-    elif len(recent) == len(coin_dic):
-        print("신규 상장 코인이 없습니다")
-    else:
-        print("사라진 코인이 있습니다")
+    if len(recent) > len(coin_cache):
         for coin in recent:
-            if coin not in coin_dic:
-                coin_dic.pop(coin)  # 사라진 코인 제거
+            if coin not in coin_cache:
+                print("[%s] - there is a new coin !! .. from checking job \tcoin : %s" % (datetime.now(), coin))
+                bucket[coin] = bithumb.get_current_price(coin)
+                coin_cache.append(coin)  # 신규 코인 추가
 
-    return "NONE"
-
-
-def buy_coin_in_bucket():
-    if len(bucket) > 0:
-        print("매수 합니다")
-        buy_if_possible()
+    elif len(recent) == len(coin_cache):
+        print("[%s] - nothing changed.. .. from checking job" % datetime.now())
 
     else:
-        print("매도할 코인이 없습니다")
+        print("[%s] - a coin disappear... .. from checking job" % datetime.now())
+        for coin in coin_cache:
+            if coin not in recent:
+                coin_cache.remove(coin)  # 사라진 코인 제거
 
 
-def sell_coin_in_bucket():
-    if len(bucket) > 0:
-        print("매도합니다")
-        sell_if_possible()
-    else:
-        print("매도할 코인이 없습니디")
-
-
-def buy_if_possible():
-    for coin in bucket:
-        recent_price = bithumb.get_current_price(coin)
-        total_krw = bithumb.get_balance(coin)[2]
-
-        lower_bound = 1.2 * bucket[coin]
-        if recent_price >= lower_bound:
-            if total_krw >= lower_bound:
-                buy(total_krw, recent_price)
-
-
-def buy(total_krw, recent_price):
-    for coin in bucket:
-        unit = total_krw / recent_price
-        bithumb.buy_market_order(coin, unit)
-
-
-def sell_if_possible():
-    for coin in bucket:
-        recent_price = bithumb.get_current_price(coin)
-        if recent_price >= 2.0 * bucket[coin]:
-            unit = bithumb.get_balance(coin)[0]
-            sell(unit)
-
-
-def sell(unit):
-    for coin in bucket:
-        bithumb.sell_market_order(coin, unit)
-
-
+##############################################################################################################################
 def get_all():
-    print('Tick! The time is: %s' % datetime.now())
-
     coins = bithumb.get_tickers()
-    print(coins)
+
+    print("[%s] - # of remote : %d \t|\t # in Cache : %d" % (datetime.now(), len(coins), len(coin_cache)))
     return coins
+
+
+##############################################################################################################################
+def make_a_deal():
+    if len(bucket) == 0:
+        return
+
+    for coin in list(bucket):  # thread - safe
+        print("[%s] - there is a coin to deal \t bucket : %s" % (datetime.now(), bucket))
+        balance = bithumb.get_balance(coin)
+
+        if balance[0] == 0:  # 신규 coin 개수가 0 -> 아직 구입하지 않음.
+            buy_if_possible(balance[2], coin)
+
+        else:
+            sell_if_possible(balance[0], coin)
+
+
+## BUYING FUNCTION############################################################################################################################
+
+def buy_if_possible(total_krw, coin):
+    recent_price = bithumb.get_current_price(coin)
+    price_to_buy = times_for_buy * bucket[coin]
+
+    if recent_price >= price_to_buy and total_krw >= price_to_buy:
+        unit = total_krw / recent_price
+        print("[%s] - buy\tname: %s, unit %d" % (datetime.now(), coin, unit))
+        return bithumb.buy_market_order(coin, unit)
+
+
+## SELLING FUNCTIONS#####################################################################################################################
+def sell_if_possible(unit, coin):
+    recent_price = bithumb.get_current_price(coin)
+
+    if recent_price >= times_for_sell * bucket[coin]:  # 코인 값이 상장가의 2배가 되는 경우 매도.
+        print("[%s] - sell\tname : %s unit : %d" % (datetime.now(), coin, unit))
+        bithumb.sell_market_order(coin, unit)
+        bucket.clear()  # clear bucket after
+
+##############################################################################################################################
--- main.py
+++ main.py
@@ -6,22 +6,25 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 app = Sanic()
 
+
 @app.listener('before_server_start')
 async def initialize_scheduler(app, loop):
     scheduler = AsyncIOScheduler({'event_loop': loop})
-    scheduler.add_job(check_new_coin, 'interval', seconds=1)
-    scheduler.add_job(buy_coin_in_bucket, 'interval', seconds=1)
-    scheduler.add_job(sell_coin_in_bucket, 'interval', seconds=1)
+
+    scheduler.add_job(check_new_coin, 'interval', seconds=1, max_instances=2)
+    scheduler.add_job(make_a_deal, 'interval', seconds=2, max_instances=2)
 
     scheduler.start()
 
+
 @app.route("/json")
 def post_json(request):
-    return json({ "received" : True , "message": request.json})
+    return json({"received": True, "message": request.json})
+
 
 @app.route("/query_srting")
 def query_string(request):
-    return json({ "parsed": True, "args": request.args, "url": request.url, "query_string": request.query_string})
+    return json({"parsed": True, "args": request.args, "url": request.url, "query_string": request.query_string})
 
 
 @app.route("/files")
@@ -34,49 +37,59 @@ def post_dto(request):
         'type': test_file.type
     }
 
-    return json({"received" : True, "file_names" : request.files.key(), "test_file_parameters": file_parameters})
+    return json({"received": True, "file_names": request.files.key(), "test_file_parameters": file_parameters})
 
 
-@app.route('/youshallnotpass') 
+@app.route('/youshallnotpass')
 async def no_no(request):
     abort(401)
-    #this won't happen
+    # this won't happen
     text('ok')
+
 
 @app.route('/')
 async def test(request):
     return json({"hello": "world"})
 
+
 @app.route('/tag/<tag>')
 async def tag_handler(request, tag):
-    return text('Tag - {}' .format(tag))
+    return text('Tag - {}'.format(tag))
+
 
 @app.route('/number/<integer_arg:int>')
 async def integer_handler(request, integer_arg):
-    return text('Number - {}' .format(integer_arg))
+    return text('Number - {}'.format(integer_arg))
+
 
 @app.route('/number/<number_arg:number>')
 async def number_handler(reqeust, number_arg):
-    return text('Number - {}' .format(number_arg))
+    return text('Number - {}'.format(number_arg))
+
 
 @app.route('/person/<name:[A-z]+>')
 async def person_handler(request, name):
-    return text('Person - {}' .format(name))
+    return text('Person - {}'.format(name))
+
 
 @app.route('/folder/<folder_id:[A-z0-9]{0,4}>')
 async def folder_handler(reqeust, folder_id):
-    return text('Folder - {}' .format(folder_id))
+    return text('Folder - {}'.format(folder_id))
+
 
 @app.route('/post', methods=['POST'])
 async def post_handler(request):
-    return text('POST request - {}' .format(request.json))
+    return text('POST request - {}'.format(request.json))
+
 
 @app.route('/get', methods=['GET'])
 async def get_handler(request):
-    return text('GET request - {}' .foramt(request.args))
+    return text('GET request - {}'.foramt(request.args))
+
 
 async def handler1(request):
     return text('OK')
+
 
 app.add_route(handler1, '/test')
 
